85debeed25a990a1a41c6cd2ea1fe7f90a0578d6
Pierre-LouisBJT <plbiojout@gmail.com>
2024-11-21 15:51:35 +0100

    Added secure route

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+import os
 from typing import Union
 from pydantic import BaseModel
 from openai import OpenAI
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = OpenAI()
 app = FastAPI()
+
+from fastapi.security.http import HTTPAuthorizationCredentials, HTTPBearer
+
+
+### (optional) Secure our API routes with an API key
+bearer = HTTPBearer()
+
+
+def get_api_hey(authorization: HTTPAuthorizationCredentials = Depends(bearer)) -> str:
+    # Parse credentials
+    api_key_token = authorization.credentials
+
+    if api_key_token != os.getenv("MY_SECRET_KEY"):
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+
+###
 
 
 # Message model
@@ -22,6 +40,25 @@ def read_root():
 
 @app.post("/message")
 def send_message(request: Message):
+    # Call OpenAI
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are Bruno Martinaud. Talk about your experience with OpenAI.",
+            },
+            {"role": "user", "content": request.message},
+        ],
+    )
+    # Extract the message from the completion
+    answer = completion.choices[0].message.content
+
+    return {"answer": answer}
+
+
+@app.post("/message-secure")
+def send_message_secure(request: Message, api_key: str = Depends(get_api_hey)):
     # Call OpenAI
     completion = client.chat.completions.create(
         model="gpt-4o",
