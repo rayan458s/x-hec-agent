230416e675f5d04b5b19a57ee8d8a52f82e64d3e
Pierre-LouisBJT <plbiojout@gmail.com>
2024-11-21 15:03:01 +0100

    Added message route

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from typing import Union
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+# Message model
+class Message(BaseModel):
+    message: str
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "PLB!"}
+
+
+@app.post("/message")
+def send_message(request: Message):
+    return {"message": request.message}
